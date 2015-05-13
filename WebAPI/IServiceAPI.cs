c537b92e280f4b8c39b961329abb9759b2f611f1
adina-moldovan <adina_moldovan29@yahoo.com>
2015-05-13 17:09:19 +0300

    bla

--- WebAPI/IServiceAPI.cs
+++ WebAPI/IServiceAPI.cs
@@ -6,8 +6,10 @@ namespace WebAPI
     public interface IServiceAPI
     {
         int CreateNewAccount(string cnp, string email, string password);
-        //DataTable GetUserDetails(string cnp);
         bool UserAuthentication(string cnp, string passsword);
-       // DataTable GetDepartmentDetails();
+        void CreateDemand(string cnp, string addressee, string request, string mention);
+        void CreateCertificate(string cnp, string reason);
+        DataTable GetMarks(string cnp, string year);
+        void PayReceipt(string cnp, string subject, string teacher );
     }
 }
